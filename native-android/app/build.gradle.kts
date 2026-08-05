87c2771ae2fb6c0595a24423f7835a8ed113f6ec
tweak87 <finke.jens87@googlemail.com>
2026-08-05 22:43:02 +0300

    Align Android Java and Kotlin targets to 17

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -18,7 +18,18 @@ android {
 
     buildFeatures { compose = true }
 
-    kotlinOptions { jvmTarget = "17" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
