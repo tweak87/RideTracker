7518842d07e4738c81f7ec9ceb99823eea81b753
tweak87 <finke.jens87@googlemail.com>
2026-08-06 13:48:12 +0300

    Fix Android Compose setContent import

--- native-android/app/src/main/java/de/ridetracker/MainActivity.kt
+++ native-android/app/src/main/java/de/ridetracker/MainActivity.kt
@@ -2,7 +2,7 @@ package de.ridetracker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.setContent
+import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 
 class MainActivity : ComponentActivity() {
