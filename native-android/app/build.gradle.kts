6f990ae690d749379efb9b0a6755e7084bbe4045
tweak87 <finke.jens87@googlemail.com>
2026-08-08 12:23:12 +0200

    fix(android): support API 21 in context and theme

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
